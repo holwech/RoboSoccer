6fd2973e4ab4a77529301172c930d87a5d345f02
Student 09 Lab RoSo <lab_roso_stud09@kaon.int.rcs.ei.tum.de>
2016-11-12 16:10:54 +0100

    Just the idea of goalkeeper

--- workspace/src/master.cpp
+++ workspace/src/master.cpp
@@ -77,7 +77,7 @@ void Master::runGoalkeeper() {
     Position ballPos(ball.GetPos());
 
     ballPos = ball.GetPos();
-    double ballangle = ball.GetPhi().Deg();
+    double ballangle = (ball.GetPhi().Deg()+ball.GetPhi().Deg()+ball.GetPhi().Deg()+ball.GetPhi().Deg()+ball.GetPhi().Deg()+ball.GetPhi().Deg())/6;
     double velocityY = sin(ballangle)*ball.GetVelocity();
 
     /** sleep function in microseconds
@@ -94,7 +94,12 @@ void Master::runGoalkeeper() {
 
 
     double side = (team == "blue" || referee.GetPlayMode() == PENALTY || referee.GetPlayMode() == BEFORE_PENALTY) ? -1.3 : 1.3;
-    robo0.GotoXY(side , nextPos, 60, true);
+    //this part is use moveMs function, to let the robot move smoothly in a half circle route in the front of the goal.
+    //the const of turning is to give the robot a different rotation speed between two wheel, so it will move in an certain turning angle.
+    //the thing not sure is the value of turning const, and the connection between time, rotation speed and the distance it travel(I know it is a mutiply,
+    //but its different in reality)
+    const turning=0.9;
+    robo0.moveMs();
     while (abs(robo0.GetY() - ball.GetY()) > 0.40 && abs(robo0.GetX() - 0.4) > 0.40) {
         usleep(5000);
     }
