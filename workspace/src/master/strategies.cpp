689134eed9d85df540b73a6116e0338ced762f98
Andreas H <andreas.heimrath@mytum.de>
2017-01-12 09:44:59 +0100

    12.1.17

--- workspace/src/master/strategies.cpp
+++ workspace/src/master/strategies.cpp
@@ -4,46 +4,6 @@
  */
 
 
-void Master::strategy_offensive(){
-    double x = ball.GetPos().DistanceTo(Position(1.38,0));
-    switch(s_case){
-    case INIT:{
-        // Our Team is behind
-        cout << "Distance:" << x << endl;
-        if( x <= 0.6 ){
-            s_case = SHOOT;
-        }
-        else if( x > 0.6){
-            s_case = POSITION;
-        }
-
-    }
-    case SHOOT: {
-        if (!player[0].isBusy() && !player[1].isBusy()) {
-            send(Command(ACTION_KICK, Position(1.38, 0), 2.2), 1);
-            s_case = INIT;
-        }
-        break;
-    }
-    case POSITION:{
-        if (!player[0].isBusy() && !player[1].isBusy()){
-            send(Command(ACTION_GOTO, Position(1.2, 0), 1.5), 1);
-            s_case = INTERRUPT;
-        }
-        break;
-    }
-    case INTERRUPT: {
-        if (!player[0].isBusy() && !player[1].isBusy()){
-            send(Command(ACTION_PASS, Position(player[1].getX(),player[1].getY())), 0);
-            s_case = INIT;
-        }
-        break;
-    }
-    default:
-        cout << "No case for this case in strategy offensive" << endl;
-        break;
-    }
-}
 
 
 
