4acfdee226e1c2dc05e268fab5f61e59818d0e72
Student 12 Lab RoSo <lab_roso_stud12@axion.int.rcs.ei.tum.de>
2017-01-23 17:26:03 +0100

    goalkeeper added

--- workspace/src/player/goalkeeper_actions.cpp
+++ workspace/src/player/goalkeeper_actions.cpp
@@ -40,14 +40,11 @@ void Player::defend()
     Position Mid;
     double goalkeeperx;
 
-    if(side == RIGHT){
-        Mid = Position(1.4,0);
-        goalkeeperx =  1.39 ;//1.36
-    }
-    else{
-        Mid = Position(-1.4,0);
-        goalkeeperx =  -1.36 ;//1.36
-    }
+    //initial goalkeeper position depending on which side
+    Mid = Position(1.4*side,0);
+    goalkeeperx = 1.39*side;
+
+    //get position of ball and angle of the ball
     for(i=0;i<10;i++)
     {
         ballangle = ball.GetPhi().Deg()+ballangle/10;
@@ -60,26 +57,36 @@ void Player::defend()
         bally += ball.GetY()/10;
     }
 
+// if the ball is moving towards our gate, goalie moves. If the ball is moving far from gate, goalie goes to the initial position
+    if(ball.GetPhi().Deg()<90 && ball.GetPhi().Deg()>-90 && side)
+    {
+        goalkeepery = tan(ballangle*M_PI/180)*(goalkeeperx-ballx)+bally;
+    }
+
+    if(!(ball.GetPhi().Deg()<90 && ball.GetPhi().Deg()>-90) && !side)
+    {
+        goalkeepery = tan(ballangle*M_PI/180)*(goalkeeperx-ballx)+bally;
+    }
 
 
-    goalkeepery = tan(ballangle*M_PI/180)*(goalkeeperx-ballx)+bally;
 
 
-//    if (ball.GetPhi().Deg()>=80)
-//    {
-//        if(bally>0)
-//            goalkeepery = 0.1;
-//        else
-//            goalkeepery = -0.1;
-//    }
-//    if (ball.GetPhi().Deg()<-80)
-//    {
-//        if(bally>0)
-//            goalkeepery = 0.1;
-//        else
-//            goalkeepery = -0.1;
-//    }
-//    if (goalkeepery == bally)
+
+/*    if (ball.GetPhi().Deg()>=80||ball.GetPhi().Deg()<-80)
+    {
+        if(bally>0)
+            goalkeepery = 0.1;
+        else
+            goalkeepery = -0.1;
+    }
+*/
+    if (ball.isStopped())
+    {
+        goalkeepery = 0;
+    }
+
+
+//    if(side==RIGHT&&)
 //    {
 //        if(ballx>0.7)
 //        {
@@ -101,41 +108,18 @@ void Player::defend()
     else if(goalkeepery<-0.245)
         goalkeepery = -0.15;
 
-   // go towards the ball if the ball is somewhere near the penalty area and moving not very fast
-/*
-    if(ball->GetPos().DistanceTo(Mid)<0.5)
-    {
-        if(ball->GetVelocity()<0.005)
-        {
-            goalkeepery = bally;
-            goalkeeperx = ballx;
-        }
-    }
-*/
-    /*
-    if(ballx>1.26)
-    {
-        if(fabs(bally)<0.3)
-        {
-            if(ball->GetVelocity()<0.005)
-            {
-                goalkeepery = bally;
-                goalkeeperx = ballx;
-            }
-        }
-    }
-*/
     //if not mooving and far out, dont move
-    if(true || !(ball.GetVelocity() < 0.01 && abs(ball.GetX()) < 1.0) ){
         POS.SetX(goalkeeperx);
         POS.SetY(goalkeepery);
-        robo.GotoPos(POS, 0.7);
-    }
+        robo.GotoPos(POS, 1);
+//        robo.CruisetoXY(goalkeeperx,goalkeepery,130);
+
 
 
     //distance to move too small, dont try to move
     //delta = sqrt((robo.GetX()-goalkeeperx)*(robo.GetX()-goalkeeperx)+(robo.GetY()-goalkeepery)*(robo.GetY()-goalkeepery));
     //if(delta<0.06) robo.StopAction();
+        cout<<"ballangle"<<ball.GetPhi().Deg()<<endl;
 
 }
 
